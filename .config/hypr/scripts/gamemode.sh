5676aa9e3054864a06071dbffd6076036ab89c0a
peanut <juntinpao27@gmail.com>
2024-09-08 21:48:35 +0800

    gamemode script to disable the fancy stuffs

---
+++ .config/hypr/scripts/gamemode.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+HYPRGAMEMODE=$(hyprctl getoption animations:enabled | grep -Po '(?<=int: ).*')
+if [ "$HYPRGAMEMODE" = 1 ] ; then
+    hyprctl --batch "\
+        keyword animations:enabled 0;\
+        keyword decoration:drop_shadow 0;\
+        keyword decoration:blur:enabled 0;\
+        keyword decoration:active_opacity 1;\
+        keyword decoration:inactive_opacity 1;\
+        keyword general:gaps_in 0;\
+        keyword general:gaps_out 0;\
+        keyword general:border_size 0;\
+        keyword decoration:drop_shadow false;\
+        keyword decoration:rounding 0"
+    exit
+fi
+hyprctl reload
+
